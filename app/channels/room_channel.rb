7c2528caf0a22f9257da93957a61ee88daf3b93d
Tr. An Dương <truongan@Trs-MacBook-Pro.local>
2017-10-23 14:52:34 +0700

    update'

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,9 @@
+require 'htmlentities'
+
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     # stream_for current_user
+
     stream_from "RoomChannel_#{params[:room]}"
 
     # stream_from "RoomChannel_#{params[:room]}"
@@ -11,6 +14,7 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-  	    Chat.create! usera:data['usera'], userb:data['userb'], body:data['body']
+        coder = HTMLEntities.new
+  	    Chat.create! usera:data['usera'], userb:data['userb'], body:coder.encode(data['body'])
   end
 end
